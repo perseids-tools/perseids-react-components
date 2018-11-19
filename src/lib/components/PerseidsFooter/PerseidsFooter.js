0830a8711a07a999d2a3f2c7c4ff0d958893504d
zfletch <zfletch2@gmail.com>
2018-11-19 12:17:35 -0500

    lint code

--- src/lib/components/PerseidsFooter/PerseidsFooter.js
+++ src/lib/components/PerseidsFooter/PerseidsFooter.js
@@ -1,49 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './PerseidsFooter.css';
 
-import github from 'octicons/build/svg/mark-github.svg';
-import report from 'octicons/build/svg/report.svg';
-import twitter from './twitter.svg';
+import githubIcon from 'octicons/build/svg/mark-github.svg';
+import reportIcon from 'octicons/build/svg/report.svg';
+import twitterIcon from './twitter.svg';
 
 const renderOcticon = (href, img, title, alt) => (
   <li className="nav-item">
-    <a className="nav-link p-2" href={href} target="_blank">
+    <a className="nav-link p-2" href={href} target="_blank" rel="noopener noreferrer">
       <img className="perseids-react-components--octicon" src={img} title={title} alt={alt} />
     </a>
   </li>
 );
 
-const renderTwitter = (href) => (
+const renderTwitter = href => (
   <li className="nav-item">
-    <a className="nav-link p-2" href={href} target="_blank">
-      <img className="perseids-react-components--twitter" src={twitter} title="Twitter" alt="Twitter Icon" />
+    <a className="nav-link p-2" href={href} target="_blank" rel="noopener noreferrer">
+      <img className="perseids-react-components--twitter" src={twitterIcon} title="Twitter" alt="Twitter Icon" />
     </a>
   </li>
 );
 
-const PerseidsFooter = (props) => (
+const PerseidsFooter = ({
+  children,
+  github,
+  report,
+  twitter,
+}) => (
   <footer className="footer perseids-react-components--footer">
     <nav className="navbar navbar-light bg-light py-0 perseids-react-components--footer-nav">
       <span className="navbar-text">
         © The Perseids Project 2018
       </span>
 
-      {props.children}
+      {children}
 
       <ul className="navbar-nav my-2 my-lg-02 flex-row">
-        {props.report && renderOcticon(props.report, report, "Report Issue", "Report Icon")}
-        {props.github && renderOcticon(props.github, github, "View Source on Github", "GitHub Icon")}
-        {props.twitter && renderTwitter(props.twitter)}
+        {report && renderOcticon(report, reportIcon, 'Report Issue', 'Report Icon')}
+        {github && renderOcticon(github, githubIcon, 'View Source on Github', 'GitHub Icon')}
+        {twitter && renderTwitter(twitter)}
       </ul>
     </nav>
   </footer>
 );
 
 PerseidsFooter.defaultProps = {
+  children: false,
+  github: 'https://github.com/perseids-project',
   report: false,
-  github: "https://github.com/perseids-project",
-  twitter: "https://twitter.com/PerseidsProject",
+  twitter: 'https://twitter.com/PerseidsProject',
+};
+
+const iconType = PropTypes.oneOfType([false, PropTypes.string]);
+
+PerseidsFooter.propTypes = {
+  children: PropTypes.element,
+  github: iconType,
+  report: iconType,
+  twitter: iconType,
 };
 
 export default PerseidsFooter;
--- src/lib/components/PerseidsHeader/PerseidsHeader.js
+++ src/lib/components/PerseidsHeader/PerseidsHeader.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './PerseidsHeader.css';
 
 import perseidsLogoTransparent from './perseids-logo-transparent.png';
 
-const PerseidsHeader = (props) => (
+const PerseidsHeader = ({ children }) => (
   <header className="navbar navbar-expand-md navbar-light bg-light perseids-react-components--navbar">
     <a className="navbar-brand" href="https://www.perseids.org">
       <img className="perseids-react-components--navbar-logo-img" src={perseidsLogoTransparent} title="perseids logo" alt="perseids logo" />
     </a>
-    {props.children}
+    {children}
   </header>
 );
+
+PerseidsHeader.defaultProps = {
+  children: false,
+};
+
+PerseidsHeader.propTypes = {
+  children: PropTypes.element,
+};
 
 export default PerseidsHeader;
