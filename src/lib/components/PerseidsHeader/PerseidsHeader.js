154f75a52018f74f2fcfe34c5b7aa3c8733c69e5
zfletch <zfletch2@gmail.com>
2019-01-30 14:51:33 -0500

    Revert "update header size css"
    This reverts commit c01e1fa694295af4cfde921c29746586364c4194.

--- src/lib/components/PerseidsHeader/PerseidsHeader.js
+++ src/lib/components/PerseidsHeader/PerseidsHeader.js
@@ -6,7 +6,7 @@ import './PerseidsHeader.css';
 import perseidsLogoTransparent from './perseids-logo-transparent.png';
 
 const PerseidsHeader = ({ children }) => (
-  <header className="navbar navbar-expand-md navbar-light bg-light">
+  <header className="navbar navbar-expand-md navbar-light bg-light perseids-react-components--navbar">
     <a className="navbar-brand perseids-react-components--navbar-brand" href="https://www.perseids.org">
       <img className="perseids-react-components--navbar-logo-img" src={perseidsLogoTransparent} title="perseids logo" alt="perseids logo" />
     </a>
