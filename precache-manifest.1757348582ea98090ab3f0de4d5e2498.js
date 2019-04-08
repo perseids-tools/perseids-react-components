7677e0b6d14d995d8b474c5220af7b058408274d
zfletch <zfletch2@gmail.com>
2019-04-08 16:54:42 -0400

    Updates

---
+++ precache-manifest.1757348582ea98090ab3f0de4d5e2498.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbf3264791891f3aeaab",
+    "url": "/perseids-react-components/static/css/index.css"
+  },
+  {
+    "revision": "dbf3264791891f3aeaab",
+    "url": "/perseids-react-components/index.js"
+  },
+  {
+    "revision": "49bddd5831c32ce84a21",
+    "url": "/perseids-react-components/static/css/1.538c5ce5.chunk.css"
+  },
+  {
+    "revision": "49bddd5831c32ce84a21",
+    "url": "/perseids-react-components/1.index.js"
+  },
+  {
+    "revision": "5ea8de1bffdd706ddad9cab7ea6c03fd",
+    "url": "/perseids-react-components/static/media/perseids-logo-transparent.5ea8de1b.png"
+  },
+  {
+    "revision": "207687a1a5ee00eb276adf2eebf83d5e",
+    "url": "/perseids-react-components/static/media/mark-github.207687a1.svg"
+  },
+  {
+    "revision": "9d8783cd007527c3962734077b258bf8",
+    "url": "/perseids-react-components/static/media/report.9d8783cd.svg"
+  },
+  {
+    "revision": "2ac2d65c948103772206c3906fe78b11",
+    "url": "/perseids-react-components/static/media/twitter.2ac2d65c.svg"
+  },
+  {
+    "revision": "cc8ecfc73730e2f3d73d5f31f937f9f8",
+    "url": "/perseids-react-components/index.html"
+  }
+];
