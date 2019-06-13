f495761afbe57c541c466bc285cbba534da069ca
zfletch <zfletch2@gmail.com>
2019-06-13 13:51:10 +0100

    Updates

---
+++ precache-manifest.ee251834fecd997fd4902582227bd9bc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0b7e1ce95fc6bf01b329e53f95e32a",
+    "url": "/perseids-react-components/index.html"
+  },
+  {
+    "revision": "31944025a244379f6724",
+    "url": "/perseids-react-components/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "79a4fa126d10057588ca",
+    "url": "/perseids-react-components/static/css/main.96bc913a.chunk.css"
+  },
+  {
+    "revision": "31944025a244379f6724",
+    "url": "/perseids-react-components/static/js/2.31944025.chunk.js"
+  },
+  {
+    "revision": "79a4fa126d10057588ca",
+    "url": "/perseids-react-components/static/js/main.79a4fa12.chunk.js"
+  },
+  {
+    "revision": "a9f7d0e09a187fce036a",
+    "url": "/perseids-react-components/static/js/runtime~main.a9f7d0e0.js"
+  },
+  {
+    "revision": "207687a1a5ee00eb276adf2eebf83d5e",
+    "url": "/perseids-react-components/static/media/mark-github.207687a1.svg"
+  },
+  {
+    "revision": "5ea8de1bffdd706ddad9cab7ea6c03fd",
+    "url": "/perseids-react-components/static/media/perseids-logo-transparent.5ea8de1b.png"
+  },
+  {
+    "revision": "9d8783cd007527c3962734077b258bf8",
+    "url": "/perseids-react-components/static/media/report.9d8783cd.svg"
+  },
+  {
+    "revision": "2ac2d65c948103772206c3906fe78b11",
+    "url": "/perseids-react-components/static/media/twitter.2ac2d65c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/perseids-react-components/precache-manifest.e5c168049205380dc8eda5cc7e81dcb1.js"
+  "/perseids-react-components/precache-manifest.ee251834fecd997fd4902582227bd9bc.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/perseids-react-components/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/perseids-react-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
